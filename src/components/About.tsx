113b096d0be1537f06046afcfcffa985c3ebd7c7
thophogie <perteztho@outlook.com>
2025-08-16 22:57:29 +0800

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -62,7 +62,7 @@ const About: React.FC = () => {
       }))
     : cards;
   return (
-    <section id="about" className="py-20">
+    <section id="about" className="py-20 bg-blue-950">
       <div className="container mx-auto px-6">
         <div className="text-center mb-16">
           <Link 
