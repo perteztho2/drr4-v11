2c18e206b5496163a1e749ed237b7ee135c3998c
thopertezproton <thopertez@protonmail.com>
2025-08-18 19:43:51 +0800

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -124,7 +124,7 @@ const Hero: React.FC<HeroProps> = ({ onEmergencyClick, onIncidentClick }) => {
 
         {/* Scroll Indicator */}
         <div className="absolute bottom-2 left-0 right-0 flex justify-center z-30">
-          <ChevronDown className="text-white animate-bounce" size={32} />
+          <ChevronDown href="google.com" className="text-white animate-bounce" size={32} />
         </div>
       </div>
       
