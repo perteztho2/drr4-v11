a8927cec998f54300d509a58a6f6a10bf45c0044
perteztho2 <thopertez@zohomail.com>
2025-08-21 19:23:28 +0800

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -278,7 +278,7 @@ const Navigation: React.FC<NavigationProps> = ({ variant = 'public' }) => {
 
           {/* Mobile Navigation */}
           {isOpen && (
-            <div className="lg:hidden py-2 border-t border-blue-800/50 backdrop-blur-xl">
+            <div className="lg:hidden py-2 border-t border-blue-800 backdrop-blur-xl">
               <div className="space-y-1">
                 {navigationTree.map((item) => renderNavigationItem(item, true))}
                 
