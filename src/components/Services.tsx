09e3b83afcf1920dec0c13c249486b6768c303dc
perteztho2 <thopertez@zohomail.com>
2025-08-21 21:34:20 +0800

    Updated Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -58,7 +58,7 @@ const Services: React.FC = () => {
       </div>
       
       <div className="container mx-auto px-4 md:px-6">
-        <div className="text-center mb-8 md:mb-16">
+        <div className="text-center mb-4 md:mb-7">
           <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold text-blue-950 mb-4 md:mb-6 relative">
             <span className="relative z-10">OUR SERVICES</span>
             <div className="absolute -bottom-1 md:-bottom-2 left-1/2 transform -translate-x-1/2 w-20 md:w-32 h-0.5 md:h-1 bg-gradient-to-r from-blue-600 to-blue-800 rounded-full"></div>
