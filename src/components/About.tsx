f96cc476d5a1043bed560b818d7681b120d4eef2
perteztho2 <thopertez@zohomail.com>
2025-08-21 20:18:38 +0800

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -62,10 +62,10 @@ const About: React.FC = () => {
       }))
     : cards;
   return (
-    <section id="about" className="relative py-7 md:py-7 bg-gradient-to-br from-blue-950 via-blue-900 to-blue-800 overflow-hidden">
+    <section id="about" className="relative py-3 md:py-3 bg-gradient-to-br from-blue-950 via-blue-900 to-blue-800 overflow-hidden">
       {/* Background Elements */}
       <div className="absolute inset-0 opacity-20">
-        <div className="absolute top-5 left-5 w-32 md:w-50 h-32 md:h-50 bg-yellow-500 rounded-full mix-blend-multiply filter blur-xl animate-pulse"></div>
+        <div className="absolute top-3 left-3 w-20 md:w-50 h-32 md:h-50 bg-yellow-500 rounded-full mix-blend-multiply filter blur-xl animate-pulse"></div>
         <div className="absolute top-5 right-5 w-32 md:w-50 h-32 md:h-50 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl animate-pulse animation-delay-2000"></div>
         <div className="absolute -bottom-4 left-10 w-32 md:w-72 h-32 md:h-50 bg-purple-400 rounded-full mix-blend-multiply filter blur-xl animate-pulse animation-delay-4000"></div>
       </div>
