2281d0e0a45c796ac9e347a3a8f7500b4b61c441
perteztho2 <thopertez@zohomail.com>
2025-08-21 21:37:44 +0800

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,24 @@ export default {
   },
   plugins: [],
 };
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        underline: {
+          '0%': { width: '0' },
+          '100%': { width: '8rem' }, // adjust to match your desired width
+        },
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.8s ease-out forwards',
+        'underline': 'underline 1s ease-out forwards',
+      },
+    },
+  },
+}
+
